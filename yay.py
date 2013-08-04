01fd0a74f8cb7aa594c94a3faee4df1ce9070adc
vayan <vaillant.y@gmail.com>
2013-08-04 15:47:20 +0200

    add textteaser api

--- yay.py
+++ yay.py
@@ -3,17 +3,20 @@
 import ircbot
 import re
 import sys
-import random
 import urlparse
 import urllib2
-from bs4 import BeautifulSoup
+import unirest
+from goose import Goose
 
 server = sys.argv[1]
 channel = sys.argv[2]
+apikey = sys.argv[3]
+
 
 class yay(ircbot.SingleServerIRCBot):
     def __init__(self):
         ircbot.SingleServerIRCBot.__init__(self, [(server, 6667)], "yay", "yet another yppy")
+        self.lastur = "https://www.google.com"
 
     def on_welcome(self, serv, ev):
         serv.join(channel)
@@ -27,13 +30,24 @@ class yay(ircbot.SingleServerIRCBot):
         if url:
             url = url.group(0)
             try:
+                self.lasturl = url
                 hostname = urlparse.urlparse(url).hostname
+                g = Goose()
+                article = g.extract(url=url)
                 tinyurl = urllib2.urlopen("http://tinyurl.com/api-create.php?url=" + url).read()
-                soup = BeautifulSoup(urllib2.urlopen(url))
-                title = soup.title.string.encode('utf-8')[:50]
+                title = article.title.encode('utf-8')[:70]
                 ret = "Title : %s (%s) | %s" % (title, hostname, tinyurl)
                 serv.privmsg(canal, ret)
-            except :
+            except:  # todo log error
+                return
+        if "!sum" in message:
+            try:
+                g = Goose()
+                article = g.extract(url=self.lasturl)
+                response = unirest.post("https://textteaser.p.mashape.com/api", {"X-Mashape-Authorization": apikey}, {"text": article.cleaned_text, "title":  article.title})
+                for bullet in response.body['sentences']:
+                    serv.privmsg(canal, ("* %s" % (bullet).encode('utf-8')))
+            except:  # todo log error
                 return
 
 if __name__ == "__main__":
